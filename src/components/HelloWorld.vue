16a6940258fd7de5b859134727cc16f4745f5fb0
yim666 <yimengruhun@gmail.com>
2023-03-16 20:04:14 +0800

    webNovel downLoad

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div>
+        <div style="background-color: rgba(255,248,230,0.93)">
+            <div class="content" v-html="fileContent"></div>
+
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    // import axios from "core-js/internals/queue";
+
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                fileContent: '',
+                filePath: 'txtcontent/novel2.txt'
+            };
+        },
+        methods:{
+          repa(text){
+              let formatted = '';
+              let dotCount = 0;
+              let exclamationCount = 0;
+
+              for (let i = 0; i < text.length; i++) {
+                  const char = text[i];
+                  if (char === '。') {
+                      dotCount++;
+                  }
+
+                  formatted += char;
+                  if (dotCount + exclamationCount === 3) {
+                      if(text[i+1] ==='”' ||(text[i+1] == '<' &&text[i+2] =='b')) continue;
+                      formatted += '<br/>';
+                      dotCount = 0;
+                      exclamationCount = 0;
+                  }
+
+              }
+              return formatted
+          }
+        },
+        async created() {
+            try {
+                const response = await fetch(this.filePath)
+                let text = await response.text()
+                text =this.repa(text)
+                //以漫开头 紧跟着 多个字符(\S*) 然后一个空格(\s+) 再多个字符 再一个空格
+                text = text.replace(/漫威里的机械狂潮\S*\s+\S*\s+/g, '<br><br><h2>$&</h2>')
+
+                // // 将双引号包裹的内容转换为带换行的段落
+                text = text.replace(/“(.*?)”/g, '<p>"$1"</p>')
+                // // 将双空格转换为换行符
+                // text = text.replace(/ {2}/g, '<br/>')
+
+
+                this.fileContent = text
+            } catch (error) {
+                console.error(error)
+            }
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    .content {
+        width: 60%;
+        margin: 0 auto;
+        text-align: left;
+        font-size: 22px;
+        color: #333;
+        line-height: 2.2;
+        letter-spacing: 1.5px;
+    }
 </style>
